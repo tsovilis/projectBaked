dc1f48dcdb7f83bca0679cb368608b2a8043b9d7
Julien Benistant <julien12_4@hotmail.com>
2012-01-25 14:27:37 +0000

    session

---
+++ Baked/winkelwagen.php
@@ -0,0 +1,57 @@
+<?php
+// Include functions
+require_once('inc/functions.inc.php');
+// Start the session
+session_start();
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<script type="text/javascript">
+
+/***********************************************
+* Textarea Maxlength script- © Dynamic Drive (www.dynamicdrive.com)
+* This notice must stay intact for legal use.
+* Visit http://www.dynamicdrive.com/ for full source code
+***********************************************/
+
+function ismaxlength(obj){
+var mlength=obj.getAttribute? parseInt(obj.getAttribute("maxlength")) : ""
+if (obj.getAttribute && obj.value.length>mlength)
+obj.value=obj.value.substring(0,mlength)
+}
+</script>
+
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<title>Baked!</title>
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<link href="baked.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+<div id="main">
+	<a href="infoBaked.html"><img src="images/Bakedsign.png" alt=""/></a>
+
+	<div id="content">
+		
+		<div id="totheleft">
+<?php include ("snelmenuBaked.html"); ?>
+		</div>
+		
+		<div id="rightside">	
+
+<?php include ("loginform.php"); ?>
+		</div>
+		
+		<div id="inhoud">
+		
+		<?php
+		echo writeShoppingCart();
+		?>
+</div>
+</div>
+</div>
+</body>
+
+</html>
