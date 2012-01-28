3a48d7d6554a579e666d600271a9780e1bdd12c9
Ruben de Vries <rubendevries@vpn-144-43.vpn.uva.nl>
2012-01-28 11:48:48 +0100

    aight

--- Baked/winkelwagen.php
+++ Baked/winkelwagen.php
@@ -93,76 +93,7 @@ obj.value=obj.value.substring(0,mlength)
 		include ("closedb.php");
 		
 		
-		//Winkelwagen.Account_id, Taarten.Taartnaam, Winkelwagen.Tekst, Winkelwagen.Aantal, Winkelwagen.Kaarsjes, Taarten.Prijs, Winkelwagen.Leverdatum	
-		
-		
-		/*		
-		echo writeShoppingCart();
-		echo "<br />";
-		
-		echo "Controleer hoeveelheden:";
-		echo showCart();
-		*/
-		
-		/*
-$cart = $_SESSION['cart'];
-$action = $_GET['action'];
-switch ($action) {
-	case 'add':
-		if ($cart) {
-			$cart .= ','.$_GET['id'];
-		} else {
-			$cart = $_GET['id'];
-		}
-		break;
-	case 'delete':
-		if ($cart) {
-			$items = explode(',',$cart);
-			$newcart = '';
-			foreach ($items as $item) {
-				if ($_GET['Taarten_id'] != $item) {
-					if ($newcart != '') {
-						$newcart .= ','.$item;
-					} else {
-						$newcart = $item;
-					}
-				}
-			}
-			$cart = $newcart;
-		}
-		break;
-	case 'update':
-	if ($cart) {
-		$newcart = '';
-		foreach ($_POST as $key=>$value) {
-			if (stristr($key,'qty')) {
-				$id = str_replace('qty','',$key);
-				$items = ($newcart != '') ? explode(',',$newcart) : explode(',',$cart);
-				$newcart = '';
-				foreach ($items as $item) {
-					if ($id != $item) {
-						if ($newcart != '') {
-							$newcart .= ','.$item;
-						} else {
-							$newcart = $item;
-						}
-					}
-				}
-				for ($i=1;$i<=$value;$i++) {
-					if ($newcart != '') {
-						$newcart .= ','.$id;
-					} else {
-						$newcart = $id;
-					}
-				}
-			}
-		}
-	}
-	$cart = $newcart;
-	break;
-}
-$_SESSION['cart'] = $cart;
-*/
+
 		?>
 		
 		</div>
