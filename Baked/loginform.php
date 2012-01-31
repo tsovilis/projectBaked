24d1efdbcebe4c45d0ed273b9d56eff71d402b29
Ruben de Vries <rubendevries@vpn-144-199.vpn.uva.nl>
2012-01-31 10:55:49 +0100

    fixed

--- Baked/loginform.php
+++ Baked/loginform.php
@@ -2,7 +2,6 @@
 
 include("verbinding1.php");
 
-
 # catch index error
 # error_reporting(E_ALL ^ E_NOTICE);
 
@@ -10,17 +9,6 @@ if(isset($_SESSION['email'])){
 	echo('<a href="accountBaked.php">Account</a><br />');
 	echo('<a href="winkelwagen.php">Winkelwagen</a><br />');
 	echo('<a href="logout.php">Uitloggen</a><br />');
-	if($_SESSION['email']=='Admin')
-	{
-		echo "<div style='height:10px'></div>
-        <div class='lijntje'></div>
-        <div style='height:10px'></div>
-
-		<strong>Administrator opties</strong>
-		<br /><br />
-		<a href='adminBaked.php'>De bestellingen lijst</a> <br />
-		<a href='admintaarttoevoegenBaked.php'>Taarten toevoegen</a>";
-	} 
 }
 
 else{
@@ -33,7 +21,7 @@ else{
 	{
 	$_POST['wachtwoord'] = "undefine";
 	}
-	
+
 	$email = $_POST["email"];
 	$wachtwoord = MD5($_POST["wachtwoord"]);
 	$result = mysql_query( "SELECT *
@@ -42,8 +30,8 @@ else{
 						AND Wachtwoord = '$wachtwoord'");
 
 	$pw = mysql_fetch_array($result);
-	
-	
+
+
     if (!$pw) {
 		print('U bent nog niet ingelogd.<br />');
 
@@ -53,28 +41,18 @@ else{
 		print('Login gelukt!<br />Klik <a href="accountBaked.php">hier</a> om verder te gaan.<br />');
 		session_start();
 		$_SESSION['email'] = $email;
-<<<<<<< HEAD
-	
 
 		if($_SESSION['email'] == 'Admin')
-			{
-			header("location:adminBaked.php");
-			}
-
-		else 
-			{
-			header("location:accountBaked.php");
-			}
-	      }
-=======
-		
-		if($email=='Admin')
+		{
 		header("location:adminBaked.php");
-		else	
-		header("location:winkelwagen.php");
+		}
+
+		else
+		{
+		header("location:accountBaked.php");
+		}
 	}
->>>>>>> f69c9c8065fcb18bb11c58a73287e8869314a96c
-	
+
 	include("closedb.php");
 
     # Switch to SSL connection if necessary.
@@ -95,7 +73,7 @@ echo <<<EOT
 <strong>Login</strong>
 </head>
 <body>
-<table border='0'>
+<table border="0">
 <form method="post" action="{$_SERVER['PHP_SELF']}">
 <tr>
 <td>E-mail</td>
