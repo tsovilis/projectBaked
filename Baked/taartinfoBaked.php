d641c0cf43c87d8d3f4501ed4b4b8741d3e3f3c0
Ruben de Vries <rubendevries@vpn-144-172.vpn.uva.nl>
2012-01-31 19:55:11 +0100

    zonder datum

--- Baked/taartinfoBaked.php
+++ Baked/taartinfoBaked.php
@@ -112,66 +112,6 @@ obj.value=obj.value.substring(0,mlength)
 							</td>
 							</select></tr>
 							
-			<tr><td colspan="2"> Leverdatum:</td></tr>
-					<tr>
-					<td width="50"><i>Jaar:</i> </td>
-					<td><select name="besteljaar">
-					<option value="2012">2012</option>
-					<option value="2013">2013</option>
-					<option value="2014">2014</option>
-					</select></td></tr>
-										
-					<tr><td><i>Maand:</i></td>
-					<td><select name="bestelmaand">
-					<option value="01">Januari</option>
-					<option value="02">Februari</option>
-					<option value="03">Maart</option>
-					<option value="04">April</option>
-					<option value="05">Mei</option>
-					<option value="06">Juni</option>
-					<option value="07">Juli</option>
-					<option value="08">Augustus</option>
-					<option value="09">September</option>
-					<option value="10">Oktober</option>
-					<option value="11">November</option>
-					<option value="12">December</option>
-					</select></td></tr>
-					
-					<tr><td><i>Dag:</i></td>
-					<td><select name="besteldag">
-					<option value="01">1</option>
-					<option value="02">2</option>
-					<option value="03">3</option>
-					<option value="04">4</option>
-					<option value="05">5</option>
-					<option value="06">6</option>
-					<option value="07">7</option>
-					<option value="08">8</option>
-					<option value="09">9</option>
-					<option value="10">10</option>
-					<option value="11">11</option>
-					<option value="12">12</option>
-					<option value="13">13</option>
-					<option value="14">14</option>
-					<option value="15">15</option>
-					<option value="16">16</option>
-					<option value="17">17</option>
-					<option value="18">18</option>
-					<option value="19">19</option>
-					<option value="20">20</option>
-					<option value="21">21</option>
-					<option value="22">22</option>
-					<option value="23">23</option>
-					<option value="24">24</option>
-					<option value="25">25</option>
-					<option value="26">26</option>
-					<option value="27">27</option>
-					<option value="28">28</option>
-					<option value="29">29</option>
-					<option value="30">30</option>
-					<option value="31">31</option>
-					</select></td></tr>
-					</table>
 			
 			
 			<div class='center'><input type="submit" name="<?php $row['Taarten_id'] ?>"></div>
