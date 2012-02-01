720566cb623d33db6cbbea88e4958cb759f59403
Ruben de Vries <rubendevries@wcw-stud-251-5.wireless.uva.nl>
2012-02-01 14:10:48 +0100

    check

--- Baked/registratie.php
+++ Baked/registratie.php
@@ -1,6 +1,52 @@
 <?php
 include ("verbinding1.php"); 
 
+if(!($_POST[voornaam]))
+	{
+	header ("Location: welcomeBaked.html");
+	}
+
+if(!($_POST[achternaam]))
+	{
+	header ("Location: welcomeBaked.html");
+	}
+
+if(!($_POST[wachtwoord]))
+	{
+	header ("Location: foutmelding.php");
+	}
+
+if(!($_POST[postcode]))
+	{
+	header ("Location: foutmelding.php");
+	}
+
+if(!($_POST[straatnaam]))
+	{
+	header ("Location: foutmelding.php");
+	}
+
+if(!($_POST[postbusnummer]))
+	{
+	header ("Location: foutmelding.php");
+	}
+
+if(!($_POST[plaatsnaam]))
+	{
+	header ("Location: foutmelding.php");
+	}
+
+if(!($_POST[telefoonnummer]))
+	{
+	header ("Location: foutmelding.php");
+	}
+
+if(!($_POST[emailadres]))
+	{
+	header ("Location: foutmelding.php");
+	}
+
+
 	if($_POST[nieuwsbrief] == '1')
 			{
 			$nieuwsbrief = 1;;
@@ -9,33 +55,34 @@ include ("verbinding1.php");
 	     $nieuwsbrief = 0;
 	    }
 	
-	$encryptedpassword = md5($_POST['wachtwoord']);
+			$encryptedpassword = md5($_POST['wachtwoord']);
 	
-	if($_POST[emailadres] == $_POST[bevestig_emailadres]  && $_POST[wachtwoord] == $_POST[bevestig_wachtwoord])
-	    {
-		$sql="INSERT INTO Account (Emailadres,Wachtwoord,Voornaam,Tussenvoegsel,Achternaam,Postcode,Straatnaam,Huisnummer,Toevoeging,Plaatsnaam,Telefoon,Nieuwsbrief) 
-		VALUES		
-		('$_POST[emailadres]','$encryptedpassword','$_POST[voornaam]','$_POST[tussenvoegselnaam]','$_POST[achternaam]','$_POST[postcode]','$_POST[straatnaam]','$_POST[postbusnummer]','$_POST[postbustoevoeging]','$_POST[plaatsnaam]','$_POST[telefoonnummer]','$nieuwsbrief')";
+			if($_POST[emailadres] == $_POST[bevestig_emailadres]  && $_POST[wachtwoord] == $_POST[bevestig_wachtwoord])
+			    {
+				$sql="INSERT INTO Account (Emailadres,Wachtwoord,Voornaam,Tussenvoegsel,Achternaam,Postcode,Straatnaam,Huisnummer,Toevoeging,Plaatsnaam,Telefoon,Nieuwsbrief) 
+				VALUES		
+				('$_POST[emailadres]','$encryptedpassword','$_POST[voornaam]','$_POST[tussenvoegselnaam]','$_POST[achternaam]','$_POST[postcode]','$_POST[straatnaam]','$_POST[postbusnummer]','$_POST[postbustoevoeging]','$_POST     [plaatsnaam]','$_POST[telefoonnummer]','$nieuwsbrief')";
 
-	    }
+			    }
 		
-	else	{ 
-		if($_POST[nieuwsbrief] == '1')
-			{
-			print"wel nieuwsbrief";
-			}
-		print "Er is een fout gemaakt. Het E-mailadres of het wachtwoord is niet hetzelfde.";
-		}
+			else	{ 
+				if($_POST[nieuwsbrief] == '1')
+					{
+					print"wel nieuwsbrief";
+					}
+				print "Er is een fout gemaakt. Het E-mailadres of het wachtwoord is niet hetzelfde.";
+				}
 
 
 
-if (!mysql_query($sql,$connection))
-  {
-  die('Error: ' . mysql_error());
-  }
+		if (!mysql_query($sql,$connection))
+		  {
+		  die('Error: ' . mysql_error());
+		  }
 
-header	("Location: accountBaked.html");
-
+		header	("Location: accountBaked.html");
+	
 
 include ("closedb.php");
+	
 ?>
