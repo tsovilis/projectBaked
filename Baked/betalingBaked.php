d4eb217c81632d9bdfc5736ba90ecd8a751e1297
Ruben de Vries <rubendevries@wcw-stud-251-5.wireless.uva.nl>
2012-02-02 14:19:36 +0100

    comment

--- Baked/betalingBaked.php
+++ Baked/betalingBaked.php
@@ -1,3 +1,5 @@
+<!-- After you orderd your cakes you get redirect to this page. This page only shows your order was orderd correctly -->
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
