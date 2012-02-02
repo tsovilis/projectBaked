d4c111d66640dd268e5bbe29a2044ba8e0f1b004
Ruben de Vries <rubendevries@vpn-145-16.vpn.uva.nl>
2012-02-02 17:25:25 +0100

    af

--- Baked/wachtwoordwijzigen.php
+++ Baked/wachtwoordwijzigen.php
@@ -6,38 +6,52 @@ include ("verbinding1.php");
 
 $email = $_POST['email'];
 
-function genRandomString() {
-    $length = 10;
-    $characters = "0123456789abcdefghijklmnopqrstuvwxyz";
-    $string = "";    
-    for ($p = 0; $p < $length; $p++) {
-        $string .= $characters[mt_rand(0, strlen($characters))];
-    }
-    return $string;
-}
+$query = mysql_query("
+		SELECT 	Account_id 
+		FROM Account 
+		WHERE Emailadres = '$email'");
 
-$newpassword = genRandomString();
-$headers  = 'MIME-Version: 1.0' . "\r\n";
-$headers .= 'From: Baked! <noreply@baked.nl>';
+	if(!(mysql_fetch_array($query)))
+		{
+		header("location:welcomeBaked.html");
+		}
+	else
+		{
+		
 
-mail($email,"WachtwoordBaked",$newpassword,$headers);
+		function genRandomString() {
+		    $length = 10;
+		    $characters = "0123456789abcdefghijklmnopqrstuvwxyz";
+		    $string = "";    
+		    for ($p = 0; $p < $length; $p++) {
+			$string .= $characters[mt_rand(0, strlen($characters))];
+		    }
+		    return $string;
+		}
+
+		$newpassword = genRandomString();
+		$headers  = 'MIME-Version: 1.0' . "\r\n";
+		$headers .= 'From: Baked! <noreply@baked.nl>';
+
+		mail($email,"WachtwoordBaked",$newpassword,$headers);
+
+		$wachtwoord = MD5($newpassword);
+
+		$sql=("	UPDATE Account 
+			SET Wachtwoord	= '$wachtwoord'
+			WHERE Emailadres = '$email'");
+
+		if (!mysql_query($sql,$connection))
+		  {
+		  die('Error: ' . mysql_error());
+		  }
+
+		// After updating you get redirected to accountBaked.php
+
+		header	("Location: wwgelukt.php");
+		
+		}
 
 
-$wachtwoord = MD5($newpassword);
-
-$sql=("	UPDATE Account 
-	SET Wachtwoord	= '$wachtwoord'
-	WHERE Emailadres = '$email'");
-
-if (!mysql_query($sql,$connection))
-  {
-  die('Error: ' . mysql_error());
-  }
-
-// After updating you get redirected to accountBaked.php
-
-header	("Location: wwgelukt.php");
-
-
-include ("closedb.php");
+		include ("closedb.php");
 ?>
