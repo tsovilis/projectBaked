95c074be8879c141ce1249d43835741f8e130211
Ivo <irhoolwerf@live.nl>
2012-02-02 20:15:22 +0100

    registratie error en validated, taartinfo en wachtwoordBaked validated en comments

--- Baked/registratieBaked.php
+++ Baked/registratieBaked.php
@@ -1,16 +1,17 @@
 <?php
   session_start();
+   ob_start();
 ?>
 
 <!-- The doctype determines the format of the page. -->
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<meta http-equiv="content-type" content="text/html; charset=utf-8" />
-<title>Baked!</title>
-<meta name="keywords" content="" />
-<meta name="description" content="" />
-<link href="baked.css" rel="stylesheet" type="text/css" />
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<title>Baked!</title>
+	<meta name="keywords" content="" />
+	<meta name="description" content="" />
+	<link href="baked.css" rel="stylesheet" type="text/css" />
 
 <script type="text/javascript">
 //<![CDATA[
@@ -19,7 +20,8 @@ function telefooncijfers()
 {
 	var telefoon=document.forms["register"]["telefoonnummer"].value;
 	if((parseFloat(telefoon) == parseInt(telefoon)) && !isNaN(telefoon)){
-		} else { 
+		} 
+	else { 
 			alert("Het telefoonnummer mag alleen cijfers bevatten.");
 			return false;
 		}
@@ -29,7 +31,8 @@ function huisnummercheck()
 {
 	var huisnummer1=document.forms["register"]["huisnummer"].value;
 	if((parseFloat(huisnummer1) == parseInt(huisnummer1)) && !isNaN(huisnummer1)){
-		} else { 
+		} 
+	else { 
 			alert("Het huisnummer mag alleen cijfers bevatten.");
 			return false;
 		}
@@ -169,47 +172,49 @@ function validate()
 <body onload="bijLaden();">
 
 <div id="main">
-	<a href="infoBaked.php"><img src="images/Bakedsign.png" alt=""/></a>
+	<a href="infoBaked.php"> <img src="images/Bakedsign.png" alt="baked teken"/></a>
 
 	<div id="content"> <!-- The content of the website will be placed in this div. -->
-		
 		<div id="totheleft">
 			<?php include ("snelmenuBaked.html"); ?> <!-- On the left side, we have the menu. -->
 		</div>
 		
 		<div id="rightside">	
-
 			<?php include ("loginform.php"); ?> <!-- On the right side we have the login. -->
-
+			
 			<div style="height:10px"></div>
 			<div class="lijntje"></div> <!-- A horizontal line to separate two parts of the right side vertically. -->
 			<div style="height:10px"></div>
 
 			<strong>Privacy Policy</strong>
-			<br /><br />
+			<p>
 			Uw persoonlijke gegevens worden veilig opgeslagen en alleen gebruikt volgens onze
 			<a href="privacypolicyBaked.php">Privacy Policy</a>.
+			</p>
 
-			<br /><br />
+			<p>
 			Baked respecteert de privacy van alle gebruikers van haar site en draagt er zorg voor dat de 
 			persoonlijke informatie die u ons verschaft vertrouwelijk wordt behandeld. 
 			Wij gebruiken uw gegevens om de bestellingen zo snel en gemakkelijk mogelijk te laten verlopen.  
 			Baked zal uw persoonlijke gegevens niet aan derden verkopen en zal deze uitsluitend aan derden 
 			ter beschikking stellen die zijn betrokken bij het uitvoeren van uw bestelling.
-
+			</p>
 		</div>
 		
 
 		
 		<div id="inhoud"> <!-- In the middle is the content of the requested page. -->
-		
+			
 			<h2>
-			Nieuw bij Baked
+				Nieuw bij Baked
 			</h2>
+			
+			<p>
 			Als u nieuw bent bij Baked, kunt u hier een account aanmaken. <br />
 			Vul hierbij uw woon of vestigingsgegevens in. <br />
 			Velden gemarkeerd met een * zijn verplicht.
-
+			</p>
+			
 			<h3>
 			Accountgegevens
 			</h3>
@@ -217,87 +222,94 @@ function validate()
 			<h4>
 			Persoonlijke gegevens
 			</h4>
+			
 			<!-- You can register here. -->
 			<form id="register" action="registratie.php" method="post"  onsubmit="return validate()">
 			<table>
 				<tr>
-				<td>Voornaam:*</td> <td><input type="text" id="voornaam" name="voornaam" /></td>
+					<td>Voornaam:*</td> <td><input type="text" id="voornaam" name="voornaam" /></td>
 				</tr>
 				
 				<tr>
-				<td>Tussenvoegsel:</td> <td><input type="text" id="tussenvoegsel" name="tussenvoegselnaam" />(bv. van der)</td>
+					<td>Tussenvoegsel:</td> <td><input type="text" id="tussenvoegsel" name="tussenvoegselnaam" />(bv. van der)</td>
 				</tr>
 				
 				<tr>
-				<td>Achternaam:*</td> <td><input type="text" id="achternaam" name="achternaam" /></td>
+					<td>Achternaam:*</td> <td><input type="text" id="achternaam" name="achternaam" /></td>
 				</tr>
 				
 				<tr>
-				<td>Postcode:*</td> <td><input type="text" id="postcode" name="postcode" />(1234AB)</td>
+					<td>Postcode:*</td> <td><input type="text" id="postcode" name="postcode" />(1234AB)</td>
 				</tr>
 				
 				<tr>
-				<td>Straatnaam:*</td> <td><input type="text" id="straatnaam" name="straatnaam" /></td>
+					<td>Straatnaam:*</td> <td><input type="text" id="straatnaam" name="straatnaam" /></td>
 				</tr>
 				
 				<tr>
-				<td>Huisnummer:*</td> <td><input type="text" id="huisnummer" name="postbusnummer" /></td>
+					<td>Huisnummer:*</td> <td><input type="text" id="huisnummer" name="postbusnummer" /></td>
 				</tr>
 				
 				<tr>
-				<td>Toevoeging:</td> <td><input type="text" id="toevoeging" name="postbustoevoeging" />(bv. a)</td>
+					<td>Toevoeging:</td> <td><input type="text" id="toevoeging" name="postbustoevoeging" />(bv. a)</td>
 				</tr>
 				
 				<tr>
-				<td>Plaatsnaam:*</td> <td><input type="text" id="plaatsnaam" name="plaatsnaam" /></td>
+					<td>Plaatsnaam:*</td> <td><input type="text" id="plaatsnaam" name="plaatsnaam" /></td>
 				</tr>
 				
 				<tr>
-				<td>Telefoonnummer:*</td> <td><input type="text" id="telefoonnummer" name="telefoonnummer" /></td>
+					<td>Telefoonnummer:*</td> <td><input type="text" id="telefoonnummer" name="telefoonnummer" /></td>
 				</tr>
 			</table>	
 
-				<br />
+			<h4>
+				Inloggegevens
+			</h4>
 				
-				<h4>Inloggegevens</h4>
-				<!-- Here you determine your login information. -->
-				Uw e-mailadres en wachtwoord heeft u nodig om toegang te krijgen tot uw gegevens.<br />
-				Tevens zullen we u via dit e-mailadres op de hoogte houden van de status van uw bestellingen.
-				<br /><br />
+			<!-- Here you determine your login information. -->
+			<p>
+			Uw e-mailadres en wachtwoord heeft u nodig om toegang te krijgen tot uw gegevens.<br />
+			Tevens zullen we u via dit e-mailadres op de hoogte houden van de status van uw bestellingen.
+			</p>
 				
-				<table>
+			<table>
 				<tr>
-				<td>E-mailadres:*</td> <td><input type="text" id="emailadres" name="emailadres" /></td>
+					<td>E-mailadres:*</td> <td><input type="text" id="emailadres" name="emailadres" /></td>
 				</tr>
 				
 				<tr>
-				<td>Bevestig e-mailadres:*</td> <td><input type="text" id="bevestigEmailadres" name="bevestig_emailadres" /></td>
+					<td>Bevestig e-mailadres:*</td> <td><input type="text" id="bevestigEmailadres" name="bevestig_emailadres" /></td>
 				</tr>
 				
 				<tr>
-				<td>Wachtwoord:*</td> <td><input type="password" id="wachtwoord" name="wachtwoord" onkeyup="pwstrength();"/> <td><p id="count">Uw wachtwoord</p></td>
+					<td>Wachtwoord:*</td> <td><input type="password" id="wachtwoord" name="wachtwoord" onkeyup="pwstrength();"/></td> <td><p id="count">Uw wachtwoord</p></td>
 				</tr>
 				
 				<tr>
-				<td>Bevestig wachtwoord:*</td> <td><input type="password" id="bevestigWachtwoord" name="bevestig_wachtwoord" /></td>
+					<td>Bevestig wachtwoord:*</td> <td><input type="password" id="bevestigWachtwoord" name="bevestig_wachtwoord" /></td>
 				</tr>
 			</table>
 
-				<h4>
+			<h4>
 				Nieuwsbrief
-				</h4>
-				<!-- Here you can decide whether or not you would like to receive the Baked! newsletter. -->
-				Blijf op de hoogte van alle acties en voordelen via de Baked Nieuwsbrief. 
-				U ontvangt het laatste nieuws direct in uw mailbox en u kunt zich op ieder gewenst moment weer uitschrijven.
-				<br /><br />
-
-				<input type="checkbox" name="nieuwsbrief" value="5" /> Ja, ik profiteer graag van alle voordelen en acties en schrijf me in voor de Baked Nieuwsbrief.
-				<br />
-				<br />
-				<br />
-				<input type="submit" value="Opsturen" />
-				<br />
-				<br />
+			</h4>
+			
+			<!-- Here you can decide whether or not you would like to receive the Baked! newsletter. -->
+			<p>
+			Blijf op de hoogte van alle acties en voordelen via de Baked Nieuwsbrief. 
+			U ontvangt het laatste nieuws direct in uw mailbox en u kunt zich op ieder gewenst moment weer uitschrijven.
+			</p>
+			
+			<p>
+			<input type="checkbox" name="nieuwsbrief" value="5" /> 
+			Ja, ik profiteer graag van alle voordelen en acties en schrijf me in voor de Baked Nieuwsbrief.
+			</p>
+			
+			<p>
+			<input type="submit" value="Opsturen" />
+			</p>
+			
 			</form>
 		</div>
 	</div>
